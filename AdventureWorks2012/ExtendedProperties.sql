7e61dbffa24103457248e8cc43be79a83c6fff03
vipin-k <vipinkumargupta@gmail.com>
2019-02-21 11:25:34 +0530

    first

---
+++ AdventureWorks2012/ExtendedProperties.sql
@@ -0,0 +1,6 @@
+﻿EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'AdventureWorks 2012 Sample OLTP Database';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Primary filegroup for the AdventureWorks 2012 sample database.', @level0type = N'FILEGROUP', @level0name = N'PRIMARY';
+
---
+++ AdventureWorks2012/Person/Tables/employee.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [person].[employee]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [emp_id] NCHAR(10) NULL, 
+    [emp_name] CHAR(10) NULL, 
+    [salary] INT NULL, 
+    [address] NCHAR(10) NULL, 
+    [phone] NCHAR(10) NULL
+)
